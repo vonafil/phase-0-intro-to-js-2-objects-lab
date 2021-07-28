10594d78547bb748d6ffb61ab280c069013da732
Alexei Lifanov <alexeilifanov@alexeis-air.myfiosgateway.com>
2021-07-27 22:03:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Joe',
+    streetAddress: '123 Elm st'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, val) {
+    let obj = Object.assign({}, employee);
+    obj[key] = val;
+    return obj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, val) {
+    obj[key] = val;
+    return obj
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let obj = Object.assign({}, employee);
+    delete obj[key];
+    return obj
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj
+}
